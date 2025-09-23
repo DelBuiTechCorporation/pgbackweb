7bb97b49ce431ee6ec99f225b2dcf8604302ad68
Victor Delbui <victor.delbui@gmail.com>
2025-09-22 22:20:32 -0300

    feat: Adiciona colunas provider e force_path_style à tabela destinations

--- internal/database/migrations/20250922215112_add_provider_and_force_path_style_to_destinations.sql
+++ internal/database/migrations/20250922215112_add_provider_and_force_path_style_to_destinations.sql
@@ -1,6 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE destinations
+ADD COLUMN IF NOT EXISTS provider TEXT NOT NULL DEFAULT 'minio',
+ADD COLUMN IF NOT EXISTS force_path_style BOOLEAN NOT NULL DEFAULT false;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE destinations
+DROP COLUMN IF EXISTS provider,
+DROP COLUMN IF EXISTS force_path_style;
+-- +goose StatementEnd-- +goose StatementBegin
+ALTER TABLE destinations
 ADD COLUMN provider TEXT NOT NULL DEFAULT 'minio',
 ADD COLUMN force_path_style BOOLEAN NOT NULL DEFAULT false;
 -- +goose StatementEnd
